73e6cbef2fa382d96c922d73fefc6a6d9efd21ff
zhourrr12345 <1477904742@qq.com>
2019-08-09 20:05:38 +0800

    1、修改原始建库SQL，令其可直接执行； 2、解决springboot启动报错的问题； 3、修改oracle的jdbc依赖包，需自行引入到本地仓库中；

--- src/main/java/cc/mrbird/Application.java
+++ src/main/java/cc/mrbird/Application.java
@@ -2,6 +2,7 @@ package cc.mrbird;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
